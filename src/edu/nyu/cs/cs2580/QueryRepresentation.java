a02f2a4e687f3587cff173a09b3249684bba4153
Solitaire <xzysolitaire@gmail.com>
2014-11-16 21:33:19 -0500

    PROTOTYPE: QuerySimilarity

--- src/edu/nyu/cs/cs2580/QueryRepresentation.java
+++ src/edu/nyu/cs/cs2580/QueryRepresentation.java
@@ -1,18 +1,39 @@
 package edu.nyu.cs.cs2580;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Vector;
 
+/*****
+ * 
+ * Provide query expansion results of the given query
+ * 
+ * @author xzysolitaire
+ *
+ */
+
 public class QueryRepresentation {
   
+  //private static final String queryFile = "/queries.tsv/"; //query file location
+  private static final String outputDir = "";    //the outputFile directory
+  
+  private String query;
+  private LinkedHashMap<String, Float> terms 
+            = new LinkedHashMap<String, Float>();
   private IndexerInvertedOccurrence indexer;
   private Vector<ScoredDocument> results = null;  //the ranked document
   private int termNum;   //restricted number of terms
   private double total_tf = 0.0; //total term frequence in the top k results
   
+  //Utilized for sorting
   class kvpair implements Comparable<kvpair> {
     public String key;
     public float value;
@@ -32,9 +53,8 @@ public class QueryRepresentation {
     }
   }
   
-  public LinkedHashMap<String, Float> QueryExpansion() {
-    LinkedHashMap<String, Float> terms = new LinkedHashMap<String, Float>();
-    
+  //Get the top M term which has the largest term probability
+  public void QueryExpansion() {
     for (ScoredDocument sd: results) {
       Document d = sd.getDocument();
       /*************
@@ -60,9 +80,9 @@ public class QueryRepresentation {
     }
     
     normalize(terms);
-    return terms;
   }
   
+  //Normalize the results 
   private void normalize(LinkedHashMap<String, Float> terms) {
     float sum = 0.0f;
     for (String key: terms.keySet()) {
@@ -74,9 +94,23 @@ public class QueryRepresentation {
     }
   }
   
-  public QueryRepresentation(Vector<ScoredDocument> results,
+  //Write the query expansion results to the output file
+  public void writeToFile() throws IOException {
+    String outputFile = outputDir + query;
+    OutputStreamWriter writer = 
+        new OutputStreamWriter(new FileOutputStream(outputFile, false));
+    
+    for (String term: terms.keySet()) {
+      writer.write(term + ' ' + terms.get(term).toString() + '\n');
+    }
+    writer.close();
+  }
+  
+  public QueryRepresentation(String query,
+                             Vector<ScoredDocument> results,
                              IndexerInvertedOccurrence indexer,
                              int termNum) {
+    this.query = query;
     this.results = results;
     this.indexer = indexer;
     this.termNum = termNum;
