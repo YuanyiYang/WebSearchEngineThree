613068333af0b716d70a80966e5632f3db51d849
Solitaire <xzysolitaire@gmail.com>
2014-11-18 20:55:39 -0500

    TODO: RankerComprehensive.java

--- src/edu/nyu/cs/cs2580/RankerComprehensive.java
+++ src/edu/nyu/cs/cs2580/RankerComprehensive.java
@@ -1,5 +1,7 @@
 package edu.nyu.cs.cs2580;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Vector;
 
 import edu.nyu.cs.cs2580.QueryHandler.CgiArguments;
@@ -11,15 +13,27 @@ import edu.nyu.cs.cs2580.SearchEngine.Options;
  * document-level features including the PageRank and the NumViews. 
  */
 public class RankerComprehensive extends Ranker {
-
+  private RankerFavorite rf; 
+  
   public RankerComprehensive(Options options,
       CgiArguments arguments, Indexer indexer) {
     super(options, arguments, indexer);
+    rf = new RankerFavorite(options, arguments, indexer);
     System.out.println("Using Ranker: " + this.getClass().getSimpleName());
   }
 
   @Override
   public Vector<ScoredDocument> runQuery(Query query, int numResults) {
+    Vector<ScoredDocument> r = rf.runQuery(query, numResults * 3);
+    Map<String, Double> pr = new HashMap<String, Double>();
+    Map<String, Double> nv = new HashMap<String, Double>();
+    
+    for (ScoredDocument sd: r) {
+      Document d = sd.getDocument();
+      pr.put(d.getUrl(), d.getPageRank());
+      
+    }
+    
     return null;
   }
 }
